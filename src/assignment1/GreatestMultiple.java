383380752dceeff25e0c4c2a3970b55fbc95d89e
jrdisser <jimdisser@altocirrusapps.com>
2018-08-08 21:40:02 -0600

    added brute force testing method

--- src/assignment1/GreatestMultiple.java
+++ src/assignment1/GreatestMultiple.java
@@ -2,7 +2,7 @@ package assignment1;
 
 public class GreatestMultiple {
 
-	public long greatestMultiple(long[] da) {
+	public long shiftSwap(long[] da) {
 		int m1 = -1;
 		int m2 = -1;
 		int t = 0;
@@ -12,25 +12,32 @@ public class GreatestMultiple {
 			return 0;
 		
 		while (i < da.length) {
-			if (m2 == -1){
+			if (m2 == -1){			//load first two numbers
 				if (m1 == -1) {
 					m1 = i;
 					++i;
 					continue;
 				} else
 					m2 = i;
+					if(da[m2] > da[m1]) {	//swap if greater
+						t = m1;
+						m1 = m2;
+						m2 = t;
+					}
+					++i;
 					continue;
 			}
 			
-			if(da[m2] > da[m1]) {
+			if(da[i] > da[m2]) {	//shift to nest index
+				m2 = i;
+			}
+			
+			if(da[m2] > da[m1]) {	//swap if greater
 				t = m1;
 				m1 = m2;
 				m2 = t;
 			}
-			
-			if(da[i] > da[m2]) {
-				m2 = i;
-			}
+
 			
 			++i;
 			
@@ -40,13 +47,33 @@ public class GreatestMultiple {
 	}
 	
 	
-	public static void main(String[] args) {
+	public long searchMax(long[] da) {
 		
-		long data[];
+		int m1 = 0;
+		int m2 = 1;			//put max in [0] and start at [1] for the 2nd loop
+		long t = 0;
 		
+		if(da.length < 2)
+			return 0;
+		
+		for( int i=0; i < da.length; ++i) {			//find max in first loop
+			if(da[m1] < da[i])
+				m1 = i;
+		}
+		
+		if(m1 != 0) {								//swap with [0]
+			t = da[0];
+			da[0] = da[m1];
+			da[m1] = t;
+		}
+		
+		for( int i=1; i < da.length; ++i) {			//find max in remaining numbers
 
-		
+			if(da[m2] < da[i])
+				m2 = i;
+		}
 
+		return da[0] * da[m2];
 	}
 
 }
