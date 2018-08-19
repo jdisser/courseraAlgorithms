7aab7097acfe8375305681d2d6c990f6c9d29044
jrdisser <jimdisser@altocirrusapps.com>
2018-08-19 15:23:01 -0600

    built fast fibonacci sum last digit

--- src/FibonacciSumLastDigit.java
+++ src/FibonacciSumLastDigit.java
@@ -21,31 +21,33 @@ public class FibonacciSumLastDigit {
     
     private static long[] getFibonacciSum(long n) {
         
-    	long[] fsr = new long[3];
+    	long[] fsr = new long[4];
     	
     	if (n == 0) {
     		fsr[0] = 0;	//the f number
     		fsr[1] = 0;	//the sum
     		fsr[2] = 0;	//the sum % 10
+    		fsr[3] = 0; //the f % 10
     		return fsr;
     	}
         
-    	if (n == 1) {
-    		fsr[0] = 1;
-    		fsr[1] = 1;
-    		fsr[2] = 1;
+   		fsr[0] = 1;
+		fsr[1] = 1;
+		fsr[2] = 1;
+    	
+    	if (n == 1) 
     		return fsr;
-    	}
+    	
         
         long fn2 = 0;
         long fn1 = 0;
         long fn0 = 1;
-        long sum = 1;
+        
 
         for (long i = 2; i <= n; ++i) {
             fn2 = fn1;
             fn1 = fn0;
-            fn0 = fn2 + fn1;
+            fn0 = (fn2 + fn1) % 10;	//same pattern?
             fsr[0] = fn0;
             fsr[1] += fn0; 
             fsr[2] = fsr[1] % 10;
@@ -55,15 +57,26 @@ public class FibonacciSumLastDigit {
         return fsr;
     }
     
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        long n = scanner.nextLong();
-        
-        long n2 = 0;
+    public static long[] getFSumSeries() {
+    	
+    	long[] fSumSeries = new long[60];
+    	
+    	fSumSeries[0] = 0;
+    	fSumSeries[1] = 1;
+    	
+    	for(int i = 2; i < 60; ++i) {
+    		fSumSeries[i] = (fSumSeries[i -1] + fSumSeries[i - 2] + 1) % 10;
+    	}
+
+    	return fSumSeries;    	
+    }
+    
+    public static void printFSeries(long n) {
+    	long n2 = 0;
         long n1 = 0;
         long n0 = 0;
         long d = 0;
-        long[] fsr = new long[3];
+        long[] fsr = new long[4];
         
         
         for(long i = 0; i <= n; ++i) {
@@ -74,13 +87,30 @@ public class FibonacciSumLastDigit {
             n1 = n0;
             n0 = fsr[2];
             
-            d = -(n0 - ((n1 + n2 + 2) % 10));
+            d = (n1 + n2 + 1) % 10;
             
             System.out.println(i + " " + fsr[0] + " " + fsr[1] + " " + fsr[2] + " " + d);
             
         }
-
-        //System.out.println(s);
+    }
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        long n = scanner.nextLong();
+        
+        long[] fSumSeries = getFSumSeries();
+        
+        int index = 0;
+        
+        if(n < fSumSeries.length) {
+        	index = (int) n;
+        } else {
+        	index = (int) (n % fSumSeries.length);
+        }
+        
+        System.out.println(fSumSeries[index]);
+        //System.out.println("Naive: " + getFibonacciSumNaive(n));
+        
         scanner.close();
     }
 }
