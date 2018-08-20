bfc7ae198b14513b5015ea1b3f9d13a075756875
jrdisser <jimdisser@altocirrusapps.com>
2018-08-19 18:20:52 -0600

    built fibonacci partial sum

--- src/FibonacciPartialSum.java
+++ src/FibonacciPartialSum.java
@@ -1,6 +1,56 @@
 import java.util.*;
 
+
+
 public class FibonacciPartialSum {
+	
+	public static long[] getFSumSeries() {
+		
+		long[] fSumSeries = new long[60];
+		
+		fSumSeries[0] = 0;
+		fSumSeries[1] = 1;
+		
+		for(int i = 2; i < 60; ++i) {
+			fSumSeries[i] = (fSumSeries[i -1] + fSumSeries[i - 2] + 1) % 10;
+		}
+
+		return fSumSeries;    	
+	}
+	
+	public static long getSumLastDigit(long n) {
+
+        
+        long[] fSumSeries = getFSumSeries();
+        
+        int index = 0;
+        
+        if(n < fSumSeries.length) {
+        	index = (int) n;
+        } else {
+        	index = (int) (n % fSumSeries.length);
+        }
+        
+        return fSumSeries[index];
+	}
+	
+	public static long getFibonacciPartialSum(long from, long to) {
+		
+		if (from == 0)
+			return getSumLastDigit(to);
+		
+		long sfrom1 = getSumLastDigit(from-1);
+		
+		long sto = getSumLastDigit(to);
+		
+		if (sfrom1 > sto) {
+			return (10 - sfrom1) + sto;
+		}
+		
+		return sto - sfrom1;
+		
+	}
+	
     private static long getFibonacciPartialSumNaive(long from, long to) {
         long sum = 0;
 
@@ -24,7 +74,10 @@ public class FibonacciPartialSum {
         Scanner scanner = new Scanner(System.in);
         long from = scanner.nextLong();
         long to = scanner.nextLong();
-        System.out.println(getFibonacciPartialSumNaive(from, to));
+        //System.out.println(getFibonacciPartialSumNaive(from, to));
+        System.out.println(getFibonacciPartialSum(from, to));
+        scanner.close();
+        
     }
 }
 
