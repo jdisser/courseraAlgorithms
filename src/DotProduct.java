0c42efec3db54e877ac3c843a58177d2921dbdfc
jrdisser <jimdisser@altocirrusapps.com>
2018-08-25 22:07:57 -0600

    built DotProduct algorithm failed tests with negative numbers and 0

--- src/DotProduct.java
+++ src/DotProduct.java
@@ -2,15 +2,55 @@ import java.util.*;
 
 public class DotProduct {
     private static long maxDotProduct(int[] a, int[] b) {
-        //write your code here
+        
+    	int n = a.length;
+    	int maxA = 0;
+    	int maxB = 0;
+    	int tA = 0;
+    	int tB = 0;
+    	int pA = 0;
+    	int pB = 0;
         long result = 0;
-        for (int i = 0; i < a.length; i++) {
+        
+        if(n == 1)
+        	return a[0] * b[0];
+    	
+    	for (int top = n - 1; top > 0; --top) {
+    		for(int j = 0; j <= top; ++j) {
+        		if(a[j] > maxA) {
+        			maxA = a[j];
+        			pA = j;
+        		}
+        		
+        		if(b[j] > maxB) {		//not handling negative numbers or 0 correctly
+        			maxB = b[j];
+        			pB = j;
+        		}
+       		
+        	}
+    		if(pA != top) {
+    			tA = a[top];
+    			a[top] = a[pA];
+    			a[pA] = tA;
+    		}
+    		
+    		if(pB != top) {
+    			tB = b[top];
+    			b[top] = b[pB];
+    			b[pB] = tB;
+    		}
+    		maxA = 0;
+    		maxB = 0;
+    	}
+ 
+        for (int i = 0; i < n; i++) {
             result += a[i] * b[i];
         }
         return result;
     }
 
     public static void main(String[] args) {
+    	
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
         int[] a = new int[n];
@@ -21,7 +61,15 @@ public class DotProduct {
         for (int i = 0; i < n; i++) {
             b[i] = scanner.nextInt();
         }
+        scanner.close();        
+        
+        System.out.println("a0: " + Arrays.toString(a));
+    	System.out.println("b0: " + Arrays.toString(b));
         System.out.println(maxDotProduct(a, b));
+    	System.out.println("a: " + Arrays.toString(a));
+    	System.out.println("b: " + Arrays.toString(b));
+        
+
     }
 }
 
