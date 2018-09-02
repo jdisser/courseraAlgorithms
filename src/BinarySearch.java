71903810b10155f451079a6277e7a39ec2abc799
jrdisser <jimdisser@altocirrusapps.com>
2018-09-02 13:00:57 -0600

    added segment class and binary search method to BinarSearch

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -3,9 +3,71 @@ import java.util.*;
 
 public class BinarySearch {
 
+	static class Segment {
+		public int left;
+		public int right;
+		
+		public Segment(int l, int r) {
+			this.left = l;
+			this.right = r;
+		}
+		
+		public int length() {
+			return right - left + 1;
+		}
+		
+		public int mid() {
+			
+			int len = right - left + 1;
+			
+			if (len < 3)
+				return left;
+			int half = len / 2;
+			return left + half;
+						
+		}
+		
+		public Segment top() {
+			int middle = this.mid();
+			return new Segment(middle, this.right);
+		}
+		
+		public Segment bottom() {
+			int middle = this.mid();
+			return new Segment(this.left, middle);
+		}
+	}
+	
+	
     static int binarySearch(int[] a, int x) {
         int left = 0, right = a.length;
-        //write your code here
+
+        if(x < a[left])
+        	return -1;
+        
+        if(x > a[right])
+        	return -1;   
+        
+        Segment sA = new Segment(left, right);
+        int lengthS = sA.length();
+
+        while (lengthS > 2) {		//not sure if this will terminate
+        	if (x == a[sA.left])
+        		return sA.left;
+        	if (x == a[sA.right])
+        		return sA.right;
+        	
+        	if (x >= a[sA.mid()])	//if equal to the mid put in top ???
+        		sA = sA.top();
+        	else
+        		sA = sA.bottom();
+        	
+        	lengthS = sA.length();
+        	
+        	//add code here to terminate the loop if the segment length is 2
+        	
+        }
+        
 
         return -1;
     }
