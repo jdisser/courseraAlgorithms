604d973a161a8e9b230ed3e4a8f3c3578d2b1a86
jrdisser <jimdisser@altocirrusapps.com>
2018-09-17 12:21:05 -0600

    fixed indek Knapsack

--- src/Knapsack.java
+++ src/Knapsack.java
@@ -7,15 +7,15 @@ public class Knapsack {
     static long optimalWeight(int W, int[] w) {
         
     	int n = w.length;
-    	long[][] val = new long [W + 1][n + 1];		//default initialized to 0
+    	long[][] val = new long [W + 1][n + 1];			//default initialized to 0
     	long tmp = 0;
     	
-    	for (int i = 1; i <= n; ++i) {				//i is the item number
+    	for (int i = 1; i <= n; ++i) {				//i is the item number (offset this -1 for array of items)
     		for( int j = 1; j <= W; ++j) {			//j is the weight for the subproblem
     			
     			val[j][i] = val[j][i - 1];			//The value without the item added
-    			if(w[i] < j) {
-    				tmp = val[j - w[i]][i - 1] + w[i];	
+    			if(w[i - 1] <= j) {
+    				tmp = val[j - w[i - 1]][i - 1] + w[i - 1];	
     				if(tmp > val[j][i])
     					val[j][i] = tmp;			//if the val (total weight of items) is larger with the item add it
     			}
